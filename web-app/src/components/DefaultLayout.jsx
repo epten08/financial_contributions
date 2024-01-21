26e9693cfc670dc22d6e41db6a3176ad78ef48e4
Arnold M S <epten08@gmail.com>
2024-01-21 21:10:51 +0200

    Revert "connected logout to backend"

--- web-app/src/components/DefaultLayout.jsx
+++ web-app/src/components/DefaultLayout.jsx
@@ -1,12 +1,10 @@
 import {Link, Navigate, Outlet} from "react-router-dom";
 import {useStateContext} from "../contexts/ContextProvider.jsx";
-import {useEffect} from "react";
-import axiosClient from "../axios-client.js";
 
 
 export default function DefaultLayout(){
 
-  const {user,token,setUser,setToken} = useStateContext()
+  const {user,token} = useStateContext()
 
   if (!token) {
     return <Navigate to="login" />
@@ -14,18 +12,7 @@ export default function DefaultLayout(){
 
   const onLogout = (ev) => {
     ev.preventDefault()
-
-    axiosClient.post('/logout').then(() => {
-      setUser({})
-      setToken(null)
-    })
   }
-
-  useEffect(() => {
-    axiosClient.get('/user').then(({data}) => {
-      setUser(data)
-    })
-  }, []);
 
   return (
     <div id="defaultLayout">
